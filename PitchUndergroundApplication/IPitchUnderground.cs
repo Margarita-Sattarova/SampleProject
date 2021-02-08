27281f67a2c8502d03ed06617f4295ffbeef2225
Margarita Sattarova <sattarova.mrgrt@gmail.com>
2021-02-08 20:18:24 +0200

    Commented out code removed

--- PitchUndergroundApplication/IPitchUnderground.cs
+++ PitchUndergroundApplication/IPitchUnderground.cs
@@ -3,7 +3,6 @@ using PitchApplication;
 
 namespace PitchUndergroundApplication {
     public interface IPitchUnderground : IPitchCommon, IUndergroundMovement {
-        //bool CanCrawl { get; set; }
         ICommand SwitchToLightCommand { get; }
         ICommand SwitchToDarkCommand { get; }
         ICommand FindWayCommand { get; }
